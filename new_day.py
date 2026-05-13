3b21c1a58a051d2f107dea971ef198b69cf8e47e
Yannick <yannickassangbe15@gmail.com>
2026-05-14 00:52:09 +0100

    first calculator version 1

---
+++ new_day.py
@@ -0,0 +1,13 @@
+print("Table de multiplication")
+for i in range(0, 13):
+    for j in range(0,13) :  
+     if j < 12 :
+        print(f" {i} fois {j} = {i*j}")
+     else :
+          print(f" {i} fois {j} = {i*j} \n")
+    if i < 12 :
+       print(f"Debut  de la table de mulptiplication de {i + 1}")
+    else :
+      print(f"merci")
+print("fin de la table de multiplication")
+# this is my firt version and enjoy your day 
